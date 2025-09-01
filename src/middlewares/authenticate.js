944110034f9a05f52895fb498cefa4be37a403c4
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-09-01 15:30:03 +0300

    get token in cookies

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,29 +3,23 @@ import { SessionsCollection } from '../db/models/sessions.js';
 import { UsersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  let token = null;
+  try {
+    const token = req.cookies.accessToken;
+    if (!token) return next(createHttpError(401, 'No access token found'));
 
-  const authHeader = req.get('Authorization');
-  if (authHeader) {
-    const [bearer, t] = authHeader.split(' ');
-    if (bearer === 'Bearer' && t) token = t;
+    const session = await SessionsCollection.findOne({ accessToken: token });
+    if (!session) return next(createHttpError(401, 'Session not found'));
+
+    if (Date.now() > session.accessTokenValidUntil) {
+      return next(createHttpError(401, 'Access token expired'));
+    }
+
+    const user = await UsersCollection.findById(session.userId);
+    if (!user) return next(createHttpError(401, 'User not found'));
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
   }
-  if (!token && req.cookies?.accessToken) {
-    token = req.cookies.accessToken;
-  }
-
-  if (!token) return next(createHttpError(401, 'No access token provided'));
-
-  const session = await SessionsCollection.findOne({ accessToken: token });
-  if (!session) return next(createHttpError(401, 'Session not found'));
-
-  if (Date.now() > session.accessTokenValidUntil) {
-    return next(createHttpError(401, 'Access token expired'));
-  }
-
-  const user = await UsersCollection.findById(session.userId);
-  if (!user) return next(createHttpError(401, 'User not found'));
-
-  req.user = user;
-  next();
 };
