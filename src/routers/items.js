e49a9edc4dd3a93c068c0232e9676018d2856c39
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-09-01 22:12:45 +0300

    add multer

--- src/routers/items.js
+++ src/routers/items.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import multer from 'multer';
 import { validateBody } from '../middlewares/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { authenticate } from '../middlewares/authenticate.js';
@@ -11,26 +12,23 @@ import { createContactMessage } from '../controllers/contactUs.js';
 import { authorizeRole } from '../middlewares/authorizeRole.js';
 
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' });
 
 router.post(
   '/addProduct',
   authenticate,
   authorizeRole('admin'),
+  upload.single('image'),
   validateBody(createSoldProductSchema),
   ctrlWrapper(createProduct),
 );
+
 router.post(
   '/contactUs',
   validateBody(createSoldProductSchema),
   ctrlWrapper(createContactMessage),
 );
-router.get('/', ctrlWrapper(getProducts));
 
-// router.patch(
-//   '/:id',
-//   authenticate,
-//   validateBody(updateTransactionSchema),
-//   ctrlWrapper(updateTransaction),
-// );
+router.get('/', ctrlWrapper(getProducts));
 
 export default router;
