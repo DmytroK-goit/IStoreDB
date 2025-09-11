ae695cfbc4aef1ccdf38dfc761b2da09b21d9db6
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-09-11 12:26:46 +0300

    repair controller add to sold item

--- src/controllers/soldItem.js
+++ src/controllers/soldItem.js
@@ -1,52 +1,36 @@
 import { OrderCollection } from '../db/models/soldItem.js';
 import { ItemsCollection } from '../db/models/items.js';
+import { CartCollection } from '../db/models/cart.js';
 
 export const createOrderFromCart = async (req, res) => {
+  const userId = req.user.id;
+  const { address } = req.body;
+  const cart = await CartCollection.findOne(
+    { userId }.populate('items.productId'),
+  );
+  if (!cart || cart.items.length === 0) {
+    return res.status(400).json({ message: 'Cart is empty' });
+  }
+
   try {
-    const userId = req.user.id;
-    const { address } = req.body;
-
-    const order = await OrderCollection.findOne({ userId, status: 'creating' });
-    if (!order || order.items.length === 0) {
-      return res.status(400).json({ message: 'Cart is empty' });
-    }
-
-    const updatedItems = [];
-    for (const item of order.items) {
-      const product = await ItemsCollection.findById(item.productId);
-      if (!product) {
-        return res
-          .status(404)
-          .json({ message: `Product ${item.productId} not found` });
-      }
-
-      if (item.quantity > product.quantity) {
-        return res.status(400).json({
-          message: `Not enough stock for product ${product.name}. Available: ${product.quantity}`,
-        });
-      }
-
-      product.quantity -= item.quantity;
-      await product.save();
-
-      updatedItems.push({
-        productId: product._id,
-        name: product.name,
-        price: product.price,
-        quantity: item.quantity,
-      });
-    }
-
-    order.items = updatedItems;
-    order.total = updatedItems.reduce(
-      (sum, item) => sum + item.price * item.quantity,
-      0,
-    );
-    order.address = address;
-    order.status = 'processing';
-    await order.save();
-
-    res.status(200).json(order);
+    const order = await OrderCollection.create({
+      userId,
+      items: cart.items.map((i) => ({
+        productId: i.productId._id,
+        name: i.productId.name,
+        price: i.productId.price,
+        quantity: i.quantity,
+      })),
+      address,
+      total: cart.items.reduce(
+        (sum, i) => sum + i.productId.price * i.quantity,
+        0,
+      ),
+      status: 'creating',
+    });
+    cart.items = [];
+    await cart.save();
+    res.status(201).json(order);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Something went wrong', error });
